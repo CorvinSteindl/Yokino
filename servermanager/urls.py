094e619792a52fb076d0d12c7f9b94b8e16752de
Thor77 <xXThor77Xx@gmail.com>
2015-12-28 18:38:15 +0100

    add working param-change

--- servermanager/urls.py
+++ servermanager/urls.py
@@ -1,10 +1,12 @@
-from django.conf.urls import url, include
+from django.conf.urls import include, url
 from django.contrib import admin
-from .views import index, server, start_server, stop_server
+
+from .views import index, params_server, server, start_server, stop_server
 
 urlpatterns = [
     url(r'^server/(?P<server_id>[0-9]+)/$', server, name='server'),
     url(r'server/(?P<server_id>[0-9]+)/start', start_server, name='start_server'),
     url(r'server/(?P<server_id>[0-9]+)/stop', stop_server, name='stop_server'),
+    url(r'server/(?P<server_id>[0-9]+)/params', params_server, name='params_server'),
     url(r'^', index, name='index')
 ]
--- servermanager/views.py
+++ servermanager/views.py
@@ -1,9 +1,10 @@
 from os import kill
-from subprocess import Popen
 from signal import SIGKILL
+from subprocess import Popen
 
 from django.contrib.auth.decorators import login_required
-from django.shortcuts import get_object_or_404, render, redirect
+from django.http import HttpResponse, HttpResponseBadRequest
+from django.shortcuts import get_object_or_404, redirect, render
 from django.utils import timezone
 
 from .models import Server, ServerInstance
@@ -46,3 +47,16 @@ def stop_server(request, server_id):
     kill(si.pid, SIGKILL)
     si.delete()
     return redirect('server', server_id)
+
+
+@login_required(login_url='login')
+def params_server(request, server_id):
+    server = get_object_or_404(Server, pk=server_id)
+    print(request.POST)
+    if 'params' in request.POST:
+        server.params = request.POST['params']
+        server.save()
+        return HttpResponse()
+    else:
+        print('cant find params')
+        return HttpResponseBadRequest()

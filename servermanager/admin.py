66d78b1c0ebc587ab61bead951d85b9b3987cb95
Thor77 <xXThor77Xx@gmail.com>
2015-12-29 13:11:23 +0100

    use select-widget instead of textinput for Server.type

--- servermanager/admin.py
+++ servermanager/admin.py
@@ -1,6 +1,25 @@
+from django import forms
 from django.contrib import admin
+
 from .models import Server, ServerInstance
+
 # Register your models here.
 
-admin.site.register(Server)
 admin.site.register(ServerInstance)
+
+
+class ServerAdminForm(forms.ModelForm):
+
+    class Meta:
+        exclude = []
+        model = Server
+        widgets = {
+            'type': forms.Select(choices=(
+                ('srcds.exe', 'CS:GO'), ('arma3server.exe', 'ARMA III'))
+            )
+        }
+
+
+@admin.register(Server)
+class ServerAdmin(admin.ModelAdmin):
+    form = ServerAdminForm
